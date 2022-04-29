d3972b7a30a0e9cb0c9a2dedfe269d57d7c773a6
asouvanlasy <asouvanlasy@outlook.com>
2022-04-29 14:31:06 -0400

    Issue: multiple commands run

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import { once, on, printConsole, Input, browser } from 'skyrimPlatform'
+import { once, on, printConsole, Input, browser, Utility } from 'skyrimPlatform'
 import * as sp from 'skyrimPlatform'
 import { ExecuteCommand } from '@skyrim-platform/console-util'
 
 browser.loadUrl('file:///Data/Platform/UI/index.html')
 
-// This event runs when this plugin is compiled
+// This event runs on hot reload
 once('tick', () => {
     browser.setVisible(false)
     browser.setFocused(false)
     printConsole('SP Hot Refresh')
 })
 
-// This event runs when any button is pressed
-on('buttonEvent', (event) => {
+// This event runs every frame (60fps)
+on('update', () => {
     if (Input.isKeyPressed(2)) {
         printConsole('opening browser')
         browser.setVisible(true)
@@ -20,16 +20,20 @@ on('buttonEvent', (event) => {
     }
 })
 
-// This event runs when it the HTML button is pressed
+// This event runs when the HTML button is pressed
 on('browserMessage', (event) => {
     let command = event.arguments
 
-    on('buttonEvent', (event) => {
+    printConsole('received: ' + command) // This appropriately receives only the most recently pressed button
+
+    // Using button event because Input.isKeyPressed isn't usable in browserMessage
+    on('buttonEvent', () => {
         if (Input.isKeyPressed(3)) {
             printConsole('executing command: ' + command)
+            ExecuteCommand(command + '') // Need to add blank string because command isn't a string (idk what type)
+
             browser.setVisible(false)
             browser.setFocused(false)
-            ExecuteCommand(command + '')
         }
     })
 })
